751b24208c660cf4f3260ad5ef9f6d72c8081ed1
Mujahi Dul Islam <51232949+MjIslamMir@users.noreply.github.com>
2022-04-12 00:41:11 +0530

    Update App.js

--- Props/App.js
+++ Props/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import Contact from './components/Contact'
+import Jokes from './components/Jokes'
 function App() {
     return (
         <div className="contacts">
@@ -27,6 +28,14 @@ function App() {
                 phone="(0800) CAT KING"
                 email="pumpkin@scrimba.com"
             />
+            <Jokes setup="I got my daughter a fridge for her birthday"
+            punchline="I can't wait to see her face light up when she opens it"/>
+            <Jokes setup="Why don't pirates travel on mountain roads?"
+            punchline="Scurvy."/>
+            <Jokes setup="Why do bees stay in the hive in the winter?"
+            punchline="Swarm."/>
+            <Jokes setup="What's the best thing about Switzerland?"
+            punchline="I don't know, but the flag is a big plus!"/>
         </div>
     )
 }
